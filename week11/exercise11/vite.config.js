d8530e774bab6f09ccf376b17144abaf9e67d9fb
Tom Reinartz <tomreinartz@Toms-MacBook-Pro.local>
2026-06-01 06:52:54 -0500

    msg

--- week11/exercise11/vite.config.js
+++ week11/exercise11/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/week11/exercise11/',
+  base: 'react_assignments4/week11/exercise11/',
 })

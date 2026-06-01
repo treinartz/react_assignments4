595558d73d833a5f93b503c210b382ab9e1b7a05
Tom Reinartz <tomreinartz@Toms-MacBook-Pro.local>
2026-06-01 06:33:22 -0500

    giter done

--- week11/exercise11/vite.config.js
+++ week11/exercise11/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/react_assignments4/',
+  base: 'https://treinartz.github.io/react_assignments4/week11/exercise11',
 })

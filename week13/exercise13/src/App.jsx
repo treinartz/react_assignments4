d42f3342dd7096efca16313ef345e1a038e91f80
Tom Reinartz <tomreinartz@Toms-MacBook-Pro.local>
2026-06-01 07:28:01 -0500

    giterdone

--- week13/exercise13/src/App.jsx
+++ week13/exercise13/src/App.jsx
@@ -1,7 +1,7 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Hello World Week13!</div>;
+  return <div className="App">Hello World Week13 again!</div>;
 }
 
 export default App;
